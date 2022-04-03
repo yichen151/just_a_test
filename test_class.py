bf5208d0f175f4a3096c11356d8f9a2c27c6ea35
Trevor <qilian227@qq.com>
2022-04-03 14:04:50 +0800

    test_first

--- test_class.py
+++ test_class.py
@@ -5,5 +5,29 @@ class FishModel:
 	def count(self):
 		print(self.num)
 
-fish = FishModel(num=10)
-fish.count()
+class HotFishModel(FishModel):
+	def __init__(self, num=0):
+		self.num = num
+		self.hot = True
+
+	def change_status(self):
+		if self.hot is True:
+			self.hot = False
+		else:
+			self.hot = True
+
+	def is_hot(self):
+		if self.hot is True:
+			print('Yes')
+		else:
+			print('No')
+
+if __name__ == '__main__':
+	fish = FishModel(num=10)
+	fish.count()
+
+	hot_fish = HotFishModel(num=20)
+	hot_fish.count()
+	hot_fish.is_hot()
+	hot_fish.change_status()
+	hot_fish.is_hot()
